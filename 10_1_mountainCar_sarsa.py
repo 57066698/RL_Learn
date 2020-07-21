c629fb01f447226933d87033c672de7da218098b
57066698 <57066698@qq.com>
2020-07-21 21:34:20 +0800

    10_1 still not work

--- 10_1_mountainCar_sarsa.py
+++ 10_1_mountainCar_sarsa.py
@@ -1,149 +1,173 @@
+"""
+    mountainCar 使用 sarsa
+"""
+
 import numpy as np
 import gym
 import torch
 import torch.nn as nn
+import math
 
-env = gym.make('MountainCar-v0')
-print(env.observation_space)
-print(env.reset())
-print(env.action_space)
-print(env.step(0))
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def sigmoid_back(y):
-    y = np.clip(y, 1e-12, 1-1e-12)
-    y = 1/y - 1
-    y = np.clip(y, 1e-12, 1-1e-12)
-    return -np.log(1/y - 1)
+"""
+    --------------   tools   --------------------
+"""
 
 # 规范s到[0, 1]
 def normalize_state(s):
     [position, velocity] = s
     pos = (position + 1.2) / 1.8
     voc = (velocity + 0.07) / 0.14
-    return pos, voc
-
-
-# model
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.dense1 = nn.Linear(5, 32)
-        self.dense2 = nn.Linear(32, 1)
-
-    def forward(self, x):
-        x = self.dense1(x)
-        x = self.dense2(x)
-        x = torch.sigmoid(x)
-        return x
-
+    return (pos, voc)
 
 def one_Hot(n, i):
     arr = np.zeros(n)
     arr[i] = 1.0
     return arr
 
+"""
+    --------------  Approximation Torch model  --------------------
+    knowing s, a
+    predict q(s, a)
+"""
 
-class QValueModel:
+
+class LinearNet(nn.Module):
     def __init__(self):
-        self.net = Net()
-        self.critrion = nn.L1Loss()
-        self.optim = torch.optim.SGD(self.net.parameters(), lr=0.01)
-        self.action_num = 3
+        super(LinearNet, self).__init__()
+        self.dense1 = nn.Linear(5, 52, bias=False)
+        self.dense2 = nn.Linear(52, 1, bias=False)
 
-    def get_x_tensor(self, s, a):
-        pos, voc = normalize_state(s)
-        x = torch.zeros((1, 5))
-        x[0, :2] = torch.Tensor([pos, voc])
-        x[0, 2:] = torch.Tensor(one_Hot(self.action_num, a))
+    def forward(self, x):
+        x = self.dense1(x)
+        x = self.dense2(x)
         return x
 
-    def get_y_tensor(self, g):
-        g = sigmoid(g)
-        y = torch.zeros((1, 1))
+class WarpModel():
+    def __init__(self):
+        self.net = LinearNet()
+        self.critrion = nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.net.parameters(), lr=0.001)
+        # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=0.9)
+
+    def train(self, x_np, y_np):
+        x_tensor = torch.Tensor(x_np).float()
+        y_tensor = torch.Tensor(y_np).float()
+        y_pred = self.net(x_tensor)
+        self.net.zero_grad()
+        loss = self.critrion(y_pred, y_tensor)
+        # if math.isnan(loss):
+        #     raise ValueError("nan")
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def predict(self, x_np):
+        x_tensor = torch.Tensor(x_np).float()
+        y_pred = self.net(x_tensor)
+        y_pred_np = y_pred.cpu().data.numpy().reshape(-1)
+        # if math.isnan(y_pred_np[0]):
+        #     raise ValueError("nan")
+        return y_pred_np
+
+"""
+    -----------------  Sarsa model  ---------------------
+    q(s, a) = q(s_next, a_next)
+"""
+class SarsaModel:
+    def __init__(self):
+        self.warpModel = WarpModel()
+        self.action_num = 3
+
+    def get_x(self, s, a):
+        pos, voc = s
+        x = np.zeros((1, 5))
+        x[0, :2] = [pos, voc]
+        x[0, 2:] = one_Hot(self.action_num, a)
+        return x
+
+    def get_y(self, g):
+        y = np.zeros((1, 1))
         y[0, 0] = g
         return y
 
-    def Qsa_value(self, s, a):
-        x_tensor = self.get_x_tensor(s, a)
-        y_tensor = self.net(x_tensor)
-        value = y_tensor.cpu().data.numpy()
-        return sigmoid_back(value[0, 0])
-
-    def train_model_sarsa(self, s, a, r, s_next = None, a_next = None):
-        Q_sa = self.Qsa_value(s, a)
+    def train_model_sarsa(self, s, a, r, s_next=None, a_next=None):
+        Q_sa = self.warpModel.predict(self.get_x(s, a))
 
         if s_next is not None and a_next is not None:
-            Q_sa_next = self.Qsa_value(s_next, a_next)
-            y = self.get_y_tensor(r+Q_sa_next-Q_sa)
+            Q_sa_next = self.warpModel.predict(self.get_x(s_next, a_next))
+            y = self.get_y(r + 0.9 * Q_sa_next - Q_sa)
         else:
-            y = self.get_y_tensor(r-Q_sa)
+            y = self.get_y(r - Q_sa)
 
-        x = self.get_x_tensor(s, a)
-        y_pred = self.net(x)
-        loss = self.critrion(y_pred, y)
-        loss.backward()
-        self.optim.step()
+        x = self.get_x(s, a)
+        loss = self.warpModel.train(x, y)
+        loss = loss.cpu().data.numpy()
         return loss
 
-    def Qs(self, s):
+    def best_action(self, s):
         """
             返回1-D数组 Q[s, a]
         """
-        position, velocity = normalize_state(s)
-        s = torch.Tensor([position, velocity])
-        x = torch.zeros((3, 5))
-        x[:, :2] = s
-        x[0, 2:] = torch.Tensor(one_Hot(3, 0))
-        x[1, 2:] = torch.Tensor(one_Hot(3, 1))
-        x[2, 2:] = torch.Tensor(one_Hot(3, 2))
-        y = self.net(x)  # [3, 1]
-        return y.cpu().data.numpy().reshape(-1)  # [N]
-
-qValueModel = QValueModel()
-num_episodes = 1000
-episode_steps = []
-epsilon = 0.1
-
-
-def choose_action(s):
-    if np.random.rand() > epsilon:
-        Qs = qValueModel.Qs(s)
-        best_action = np.argmax(Qs)
+        x0 = self.get_x(s, 0)
+        x1 = self.get_x(s, 1)
+        x2 = self.get_x(s, 2)
+        x_batch = np.concatenate((x0, x1, x2), axis=0)
+        y_batch = self.warpModel.predict(x_batch)  # [3, 1]
+        best_action = np.argmax(y_batch)
         return best_action
-    else:
-        return np.random.randint(0, 3)
+
+"""
+    ----------  main loop ----------------------
+"""
 
 
-for i in range(num_episodes):
-    # reset environment
-    s = env.reset()
-    step_count = 0
-    done = None
-    # loop until end
-    while not done:
-        # choose action
-        a = choose_action(s)
-        # play environment
-        s_, r, done, _ = env.step(a)
-        env.render()
-        step_count += 1
-        # when game end, record episode rewards
-        if done:
-            qValueModel.train_model_sarsa(s, a, r)
-            episode_steps.append(step_count)
-        else:
-            a_ = choose_action(s_)
-            qValueModel.train_model_sarsa(s, a, r, s_, a_)
+if __name__ == "__main__":
 
-            s = s_
-            a = a_
+    env = gym.make('MountainCar-v0')
+    print(env.observation_space)
+    print(env.reset())
+    print(env.action_space)
+    print(env.step(0))
 
-# plot episode_rewards
-import matplotlib.pyplot as plt
+    sarsaModel = SarsaModel()
+    num_episodes = 1000
+    end_pos = []
+    epsilon = 0.3
 
-plt.plot(episode_steps)
-plt.show()
+    for i in range(num_episodes):
+        # reset environment
+        s = env.reset()
+        s = normalize_state(s)
+        step_count = 0
+        done = None
+        # loop until end
+        while not done:
+            # choose action
+            if np.random.rand() > epsilon:
+                a = sarsaModel.best_action(s)
+            else:
+                a = np.random.randint(0, 3)
+            # play environment
+            s_, r, done, _ = env.step(a)
+            s_ = normalize_state(s_)
+            # env.render()
+            # when game end, record episode rewards
+            if done:
+                loss = sarsaModel.train_model_sarsa(s, a, r)
+                end_pos.append(s[0])
+                print("epsilon %d" % i, loss, s[0], r==1)
+            else:
+                if np.random.rand() > epsilon:
+                    a_ = sarsaModel.best_action(s)
+                else:
+                    a_ = np.random.randint(0, 3)
+                loss = sarsaModel.train_model_sarsa(s, a, r, s_, a_)
+
+                s = s_
+                a = a_
+
+    # plot episode_rewards
+    import matplotlib.pyplot as plt
+
+    plt.plot(end_pos)
+    plt.show()
